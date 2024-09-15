3ab22cbe19af17c4905f8820d66c47198ab6e853
Siddharthvj23 <vijaysiddharth23@gmail.com>
2024-09-15 13:57:10 +0530

    updated responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { TouchBackend } from 'react-dnd-touch-backend'
 import './App.css';
 import KanbanBoard from './Components/KanbanBoard';
 
 const App = () => {
+
+  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+  const Backend = isMobile ? TouchBackend : HTML5Backend
   return (
-    <DndProvider backend={HTML5Backend}>
+    <DndProvider backend={Backend}>
       <main>
         <KanbanBoard />
       </main>
